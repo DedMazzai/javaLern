ee6c8b6e63e4f51a57174a4cf0c0eb0ce0e923e6
DedMazzai <ded_mazzai@inbox.ru>
2022-03-25 17:09:11 +0300

    refactor class Point

--- sandbox/src/main/java/ru/gpb/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/gpb/pft/sandbox/Point.java
@@ -9,29 +9,28 @@ public class Point {
         this.coorY = coorY;
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.coorX - p1.coorX) * (p2.coorX - p1.coorX) + (p2.coorY - p1.coorY) * (p2.coorY - p1.coorY));
-
+    public double distance(Point p1) {
+        return Math.sqrt((this.coorX - p1.coorX) * (this.coorX - p1.coorX) +
+                (this.coorY - p1.coorY) * (this.coorY - p1.coorY));
     }
 }
 
 class PointRun {
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.coorX - p1.coorX) * (p2.coorX - p1.coorX) + (p2.coorY - p1.coorY) * (p2.coorY - p1.coorY));
+    }
+
     public static void main(String[] args) {
         Point p1 = new Point(112, 23);
         Point p2 = new Point(-3, 9);
+        Point p3 = new Point(-32, 19);
 
         System.out.println("Расстояние между точкой X с координатами: (" + p1.coorX + "; " + p1.coorY + ")" + " и " +
                 "точкой У с координатами: (" + p2.coorX + "; " + p2.coorY + ") " +
-                "составляет: - " + Point.distance(p1, p2));
-
+                "составляет: - " + p1.distance(p3));
 
         System.out.println("Расстояние между точкой X с координатами: (" + p1.coorX + "; " + p1.coorY + ")" + " и " +
-                "точкой У с координатами: (" + p1.coorX + "; " + p2.coorY + ") " +
+                "точкой У с координатами: (" + p3.coorX + "; " + p3.coorY + ") " +
                 "составляет: - " + distance(p1, p2));
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.coorX - p1.coorX) * (p2.coorX - p1.coorX) + (p2.coorY - p1.coorY) * (p2.coorY - p1.coorY));
-
     }
 }
